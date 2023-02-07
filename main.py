cfb33071bebac79b7f4c4d0476d1bc715227e472
Nagy Botond <75550932+westernwontons@users.noreply.github.com>
2023-02-07 11:01:20 +0200

    commented out main.py because the content was only for quick testing

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import os
-import asyncio
-from typing import cast
+# import os
+# import asyncio
+# from typing import cast
 
-from devtools import debug
-from client import AsyncMongoClient
+# from devtools import debug
+# from client import AsyncMongoClient
 
-DATABASE_URL = cast(str, os.getenv("MONGODB_URL"))
-
-
-async def main():
-    client = AsyncMongoClient(DATABASE_URL)
-    db = client.get_database("database")
-    example = db.get_collection("example")
-    result = await example.update_many(
-        {"user": "BB8"}, {"$set": {"user": "BB10"}}, upsert=True
-    )
-    return result
+# DATABASE_URL = cast(str, os.getenv("MONGODB_URL"))
 
 
-if __name__ == "__main__":
-    result = asyncio.run(main())
-    debug(result.matched_count)
+# async def main():
+#     client = AsyncMongoClient(DATABASE_URL)
+#     db = client.get_database("database")
+#     example = db.get_collection("example")
+#     result = await example.update_many(
+#         {"user": "BB8"}, {"$set": {"user": "BB10"}}, upsert=True
+#     )
+#     return result
+
+
+# if __name__ == "__main__":
+#     result = asyncio.run(main())
+#     debug(result.matched_count)
